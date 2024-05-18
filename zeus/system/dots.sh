27eb74f18812c4d7d7e6a5345a3768c512981b17
André Rodrigues <andre7391@gmail.com>
2024-05-18 14:19:11 -0400

    changes to system packages

--- zeus/system/dots.sh
+++ zeus/system/dots.sh
@@ -4,10 +4,16 @@
 dir=$(dirname ${BASH_SOURCE[0]})
 
 # system packages
-arch_install xorg xorg-xinit
+arch_install xorg xorg-xinit mkinitcpio-firmware
 
 # cpu
 arch_install amd-ucode
 
 # gpu
 arch_install mesa xf86-video-amdgpu vulkan-radeon libva-mesa-driver
+
+
+# configs
+copy_to_root $dir/pacman.conf /etc/pacman.conf
+copy_to_root $dir/makepkg.conf /etc/makepkg.conf
+copy_to_root $dir/mkinitcpio.conf /etc/mkinitcpio.conf
