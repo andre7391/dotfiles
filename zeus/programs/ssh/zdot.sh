26fedba81d37e3c208e0c4c97d14a650b03e32ec
André Rodrigues <andre7391@gmail.com>
2024-06-29 18:01:06 -0400

    chages to ssh keys

--- zeus/programs/ssh/zdot.sh
+++ zeus/programs/ssh/zdot.sh
@@ -4,8 +4,8 @@
 install openssh
 
 # generate ssh key
-if ! [[ -f ~/.ssh/id_ed25519_andre7391 ]] ; then
-    ssh-keygen -N "" -f ~/.ssh/id_ed25519_andre7391 -t ed25519 -C "andre7391@gmail.com"
+if ! [[ -f ~/.ssh/id_ed25519_personal ]] ; then
+    ssh-keygen -N "" -f ~/.ssh/id_ed25519_personal -t ed25519 -C "andre7391@gmail.com"
     eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_ed25519_andre7391
+    ssh-add ~/.ssh/id_ed25519_personal
 fi
