b619181dcdd7246a1002e8985da825568393f763
André Rodrigues <andre7391@gmail.com>
2024-06-29 18:17:13 -0400

    created mensurepec ssh key

--- zeus/programs/ssh/zdot.sh
+++ zeus/programs/ssh/zdot.sh
@@ -3,9 +3,19 @@
 # ssh package
 install openssh
 
-# generate ssh key
+# generate personal ssh key
 if ! [[ -f ~/.ssh/id_ed25519_personal ]] ; then
     ssh-keygen -N "" -f ~/.ssh/id_ed25519_personal -t ed25519 -C "andre7391@gmail.com"
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_ed25519_personal
 fi
+
+# generate mensurepec ssh key
+if ! [[ -f ~/.ssh/id_ed25519_mensurepec ]] ; then
+    ssh-keygen -N "" -f ~/.ssh/id_ed25519_mensurepec -t ed25519 -C "andre.rodrigues@mensurepec.com"
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519_mensurepec
+fi
+
+# symlink ssh config
+symlink $dir/config ~/.ssh/config
