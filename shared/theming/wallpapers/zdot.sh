eb67a467be44eeb4493c28af00f3b1145911c5bc
André Rodrigues <andre7391@gmail.com>
2024-12-09 08:41:39 -0400

    wallpapers change

--- shared/theming/wallpapers/zdot.sh
+++ shared/theming/wallpapers/zdot.sh
@@ -5,12 +5,12 @@ symlink $dir/wallhaven ~/.config/wallpapers/wallhaven
 symlink $dir/unplash ~/.config/wallpapers/unplash
 
 # # commands to convert images with imagemagick
-for file in * ; do
-    # echo $file
-    # file $file 
-    if [[ -f $file ]] ; then
-        file $file
-        cp $file converted/${file%%.*}-original.${file##*.}
-        magick $file -gravity center -crop 16:9 -strip -sampling-factor 4:4:4 -unsharp 0x1 -density 300 -resize x1440 -quality 95 converted/${file%%.*}.jpg
-    fi
-done
+# for file in * ; do
+#     # echo $file
+#     # file $file 
+#     if [[ -f $file ]] ; then
+#         file $file
+#         cp $file converted/${file%%.*}-original.${file##*.}
+#         magick $file -gravity center -crop 16:9 -strip -sampling-factor 4:4:4 -unsharp 0x1 -density 300 -resize x1440 -quality 95 converted/${file%%.*}.jpg
+#     fi
+# done
